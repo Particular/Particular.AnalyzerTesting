6cad5f84a31b6668c2fb234b7040a58ca1bfb567
David Boike <david.boike@gmail.com>
2026-03-05 08:57:16 -0600

    Ignore "special" values in LanguageVersion enum (#7)

--- src/Particular.AnalyzerTesting/LangVersionHelper.cs
+++ src/Particular.AnalyzerTesting/LangVersionHelper.cs
@@ -7,6 +7,7 @@ using Microsoft.CodeAnalysis.CSharp;
 static class LangVersionHelper
 {
     public static readonly LanguageVersion LatestForCurrentRoslynSdk = Enum.GetValues<LanguageVersion>()
+        .Where(value => value.ToString().StartsWith("CSharp"))
         .OrderByDescending(value => value)
         .First();
 }
